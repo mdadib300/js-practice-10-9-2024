414728b6174a8794d35b7cca6e36175fb9c03791
Md. Adib Chowdhury <mdadib300@gmail.com>
2024-09-10 11:05:50 +0600

    js practice 10/9/24

---
+++ practice.js
@@ -0,0 +1,24 @@
+// Practice task 1
+var totalMoney = 1000;
+var orangePrice = 200;
+var applePrice = 400;
+var totalCost = orangePrice+applePrice;
+var remainingMoney = totalMoney-totalCost;
+console.log(remainingMoney)
+
+// Practice task 2
+var physicsMarks = 78.36;
+var chemistryMarks = 98.365;
+var mathMarks = 80;
+var biologyMarks = 66.584;
+var banglaMarks = 98.36985;
+var totalMarks = physicsMarks+chemistryMarks+mathMarks+biologyMarks+banglaMarks;
+var averageMarks = totalMarks/5;
+var finalResult = averageMarks.toFixed(2);
+console.log(finalResult)
+
+// Practice task 3
+var n1 = 119;
+var divideBy = 5;
+var remainder = n1%divideBy;
+console.log(remainder)
